7f287659a67f88aa7d0e45940811b976a7c1b6e2
Angie Smiderle <50060168+Promethaes@users.noreply.github.com>
2023-12-06 23:15:19 -0500

    Add sample audio mixer and player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+    public UnityEvent playSound;
     // Start is called before the first frame update
     void Start()
     {
+        playSound.Invoke();
         Debug.Log("Hello World");
     }
 
