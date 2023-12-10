5f7c3e8110c10b89966fc4fae2607e1b2f0c7130
Angie Smiderle <50060168+Promethaes@users.noreply.github.com>
2023-12-10 15:47:29 -0500

    stuff

--- Assets/Scripts/BigBad.cs
+++ Assets/Scripts/BigBad.cs
@@ -12,23 +12,22 @@ public class BigBad : MonoBehaviour
 
     private void Start()
     {
-        IEnumerator Despawn()
-        {
-            yield return new WaitForSeconds(15.0f);
-            gameObject.SetActive(false);
-            GameManager.instance.OnReturnToNormal.Invoke();
-        }
-        StartCoroutine(Despawn());
 
         GameManager.instance.OnPlayerHide.AddListener(() =>
         {
             var monsterManager = FindObjectOfType<MonsterManager>();
             int index = Random.Range(0, monsterManager.monsterTravelLocations.Count);
             navMeshAgent.SetDestination(monsterManager.monsterTravelLocations[index].position);
+            StartCoroutine(Despawn());
         });
 
     }
-
+    IEnumerator Despawn()
+    {
+        yield return new WaitForSeconds(15.0f);
+        gameObject.SetActive(false);
+        GameManager.instance.OnReturnToNormal.Invoke();
+    }
     private void OnTriggerEnter(Collider other)
     {
         var player = other.gameObject.GetComponentInParent<Player>();
